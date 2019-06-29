9448b961067df25b756be366b8c61e54934181bd
gwcadmin <gwcadmin@gwcadmins-MacBook-Pro.local>
2019-06-29 17:24:18 -0400

    Load all pokemon and log to console

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
+function getAllPokemon() {
+  let result = [];
+  $.ajax({
+    url: 'http://pokeapi.co/api/v2/pokemon/?limit=900',
+    type: 'GET',
+    data: {},
+    async: false,
+    dataType: 'json',
+    success: function(data) {
+      let pokeArray = data["results"];
+      result = pokeArray;
+    },
+    error: function(request, error) {
+      alert('Request: ' + JSON.stringify(request));
+    }
+  });
+  return result;
+}
+
 $(document).ready(function() {
     console.log( "ready!" );
-    $.ajax({
-      url: 'https://pokeapi.co/api/v2/pokemon/butterfree',
-      type: 'GET',
-      data: {},
-      dataType: 'text',
-      success: function(data) {
-        console.log('Data: ' + data);
-      },
-      error: function(request, error) {
-        alert('Request: ' + JSON.stringify(request));
-      }
+
+    $('#searchbar').keyup(function() {
+      console.log($('#searchbar').val());
     });
+
+    const allPokemon = getAllPokemon();
+    console.log(allPokemon);
 });
